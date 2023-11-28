367865bd508c79d36ecb6af7264058ecdadecc5c
fukazawa-lab <147732705+fukazawa-lab@users.noreply.github.com>
2023-11-28 14:10:25 +0900

    Update create_validation_sequence.py

--- create_validation_sequence.py
+++ create_validation_sequence.py
@@ -18,54 +18,84 @@ def generate_data(infile, outfile, rep_num, seq_num):
     # 処理結果を保存するデータフレーム
     result_rows = []
 
-    for _ in range(rep_num):
-        # validation.csvの行ごとに処理
-        for index, row in df_valid.iterrows():
-            user_id = f"user_{str(int(row['userId']))}"  # ユーザーIDを整数として処理し、小数点以下があれば削除
+    # rep_num回水増しする
+    if seq_num != -1:
+        for _ in range(rep_num):
+            # validation.csvの行ごとに処理
+            for index, row in df_valid.iterrows():
+                user_id = f"user_{str(int(row['userId']))}"  # ユーザーIDを整数として処理し、小数点以下があれば削除
+                user_info = df_userinfo[df_userinfo['user'] == user_id]
+
+                # user_infoが存在する場合のみ処理を行う
+                if not user_info.empty:
+                    user_sentence = user_info['sentence'].values[0]
+
+                    # sentenceを"[SEP]"で分割
+                    user_sequence, movie_sequence, rating_sequence = user_sentence.split(SEP)
+                    movies = movie_sequence.split()
+                    ratings = rating_sequence.split()
+                    threshold = len(movies)
+                    k_num = int(threshold * 0.6)
+
+                    # ランダムにk個の整数を選ぶ
+                    selected_indices = random.sample(range(threshold), seq_num)
+
+                    # 選ばれた整数に対応する要素を取り出してリストにする
+                    selected_movies = [movies[i] for i in selected_indices]
+                    selected_ratings = [ratings[i] for i in selected_indices]
+
+                    # movieIdに対応するdescriptionを取得し、連結する
+                    selected_descriptions = []
+                    for movie_id in selected_movies:
+                        description = metadata[metadata['movieId'] == int(movie_id.split("_")[1])]['description'].values[0]
+                        selected_descriptions.append(description)
+
+                    # descriptionを"."で連結
+                    movie_description_sequence = ". ".join(selected_descriptions)
+
+                    # 2つ目の"[SEP]"の前にmovieIdを入れる
+                    movie_id = f"movie_{str(int(row['movieId']))}"
+                    target_movie_description = metadata[metadata['movieId'] == int(row['movieId'])]['description'].values[0]
+
+                    user_sequence = (
+                        user_sequence + SEP + " " + ' '.join(selected_movies) +
+                        " " + movie_id + " " + SEP + " " + ' '.join(selected_ratings) + " " + MASK +
+                        SEP +movie_description_sequence 
+                    )
+
+                    # labelにratingを追加
+                    label = row['rating']
+
+                    # 結果を保存
+                    result_rows.append({'sentence': user_sequence, 'label': label})
+    else:
+        for index, row in df_userinfo.iterrows():
+            user_id = row['user'] # ユーザーIDを整数として処理し、小数点以下があれば削除
+            print(user_id)
             user_info = df_userinfo[df_userinfo['user'] == user_id]
 
-            # user_infoが存在する場合のみ処理を行う
-            if not user_info.empty:
-                user_sentence = user_info['sentence'].values[0]
+            user_sentence = user_info['sentence'].values[0]
 
-                # sentenceを"[SEP]"で分割
-                user_sequence, movie_sequence, rating_sequence = user_sentence.split(SEP)
-                movies = movie_sequence.split()
-                ratings = rating_sequence.split()
-                threshold = len(movies)
-                k_num = int(threshold * 0.6)
+            # Iterate through each movie and create a sentence
+            # sentenceを"[SEP]"で分割
+            user_sequence, movie_sequence, rating_sequence = user_sentence.split(SEP)
+            movies = movie_sequence.split()
+            ratings = rating_sequence.split()
 
-                # ランダムにk個の整数を選ぶ
-                selected_indices = random.sample(range(threshold), seq_num)
+            for i in range(len(movies)):
+              # Create a copy of the ratings list to avoid modifying the original list
+              masked_ratings = ratings.copy()
+              # labelにratingを追加
+              label = masked_ratings[i]
 
-                # 選ばれた整数に対応する要素を取り出してリストにする
-                selected_movies = [movies[i] for i in selected_indices]
-                selected_ratings = [ratings[i] for i in selected_indices]
+              # Add [MASK] to the last element of the ratings corresponding to the current movie
+              masked_ratings[i] = "[MASK]"
 
-                # movieIdに対応するdescriptionを取得し、連結する
-                selected_descriptions = []
-                for movie_id in selected_movies:
-                    description = metadata[metadata['movieId'] == int(movie_id.split("_")[1])]['description'].values[0]
-                    selected_descriptions.append(description)
-
-                # descriptionを"."で連結
-                movie_description_sequence = ". ".join(selected_descriptions)
-
-                # 2つ目の"[SEP]"の前にmovieIdを入れる
-                movie_id = f"movie_{str(int(row['movieId']))}"
-                target_movie_description = metadata[metadata['movieId'] == int(row['movieId'])]['description'].values[0]
-
-                user_sequence = (
-                    user_sequence + SEP + " " + ' '.join(selected_movies) +
-                    " " + movie_id + " " + SEP + " " + ' '.join(selected_ratings) + " " + MASK +
-                     SEP +movie_description_sequence 
-                )
-
-                # labelにratingを追加
-                label = row['rating']
-
-                # 結果を保存
-                result_rows.append({'sentence': user_sequence, 'label': label})
+              # Create the sentence for the current movie
+              sentence = user_sequence + SEP + " " + ' '.join(movies) + " " + SEP + " " + ' '.join(masked_ratings)
+              print(sentence)
+              # 結果を保存
+              result_rows.append({'sentence': sentence, 'label': label})
 
     # 結果をデータフレームに変換
     result_df = pd.DataFrame(result_rows)
