c0e618e73e557c331fad916ad71069a6df7acbb0
fukazawa-lab <147732705+fukazawa-lab@users.noreply.github.com>
2024-06-07 16:19:26 +0900

    Update create_bert_input.py

--- src/methods/create_bert_input.py
+++ src/methods/create_bert_input.py
@@ -11,21 +11,23 @@ def create_bert_input_csv(file_name_in, file_name_out, sentence, user_profile_fi
     # user_profile_data.csvを辞書に変換します（userIdからprofileへのマッピング）
     user_profile_dict = user_profile_df.set_index('userId')['profile'].to_dict()
 
-    # metadata_all.csvを辞書に変換します（movieIdからtitleへのマッピング）
-    movie_title_dict = metadata_df.set_index('movieId')['title'].to_dict()
+    # metadata_all.csvを辞書に変換します（itemIdからtitleへのマッピング）
+    item_title_dict = metadata_df.set_index('itemId')['title'].to_dict()
 
-    # metadata_all.csvを辞書に変換します（movieIdからgenreへのマッピング）
-    movie_genre_dict = metadata_df.set_index('movieId')['genres'].to_dict()
+    # metadata_all.csvを辞書に変換します（itemIdからcategoryへのマッピング）
+    item_category_dict = metadata_df.set_index('itemId')['category'].to_dict()
 
-    # metadata_all.csvを辞書に変換します（movieIdからoverviewへのマッピング）
-    movie_overview_dict = metadata_df.set_index('movieId')['overview'].to_dict()
+    # metadata_all.csvを辞書に変換します（itemIdからdescriptionへのマッピング）
+    item_description_dict = metadata_df.set_index('itemId')['description'].to_dict()
 
-    # metadata_all.csvを辞書に変換します（movieIdからoverviewへのマッピング）
-    movie_release_date_dict = metadata_df.set_index('movieId')['release_date'].to_dict()
+    # metadata_all.csvを辞書に変換します（itemIdからdescriptionへのマッピング）
+    item_option1_dict = metadata_df.set_index('itemId')['option1'].to_dict()
 
-    # metadata_all.csvを辞書に変換します（movieIdからoverviewへのマッピング）
-    movie_runtime_dict = metadata_df.set_index('movieId')['runtime'].to_dict()
+    # metadata_all.csvを辞書に変換します（itemIdからdescriptionへのマッピング）
+    item_option2_dict = metadata_df.set_index('itemId')['option2'].to_dict()
 
+    # metadata_all.csvを辞書に変換します（itemIdからdescriptionへのマッピング）
+    item_option3_dict = metadata_df.set_index('itemId')['option3'].to_dict()
 
     # 結果を保存するリスト
     sentences = []
@@ -35,40 +37,43 @@ def create_bert_input_csv(file_name_in, file_name_out, sentence, user_profile_fi
     # CSVの各行に対して処理を行います
     for _, row in df.iterrows():
         user_id = int(row['userId'])
-        movie_id = int(row['movieId'])
+        item_id = int(row['itemId'])
         rating = row['rating']
 
         # user_profile_data.csvから該当するprofileを取得します
         profile = user_profile_dict.get(user_id, "")
 
         # metadata_all.csvから該当するtitleを取得します
-        title = movie_title_dict.get(movie_id, "")
+        title = item_title_dict.get(item_id, "")
 
-        # metadata_all.csvから該当するgenreを取得します
-        genre = movie_genre_dict.get(movie_id, "")
+        # metadata_all.csvから該当するcategoryを取得します
+        category = item_category_dict.get(item_id, "")
 
-        # metadata_all.csvから該当するoverviewを取得します
-        overview = movie_overview_dict.get(movie_id, "")
+        # metadata_all.csvから該当するdescriptionを取得します
+        description = item_description_dict.get(item_id, "")
 
-        # metadata_all.csvから該当するoverviewを取得します
-        release_date = movie_release_date_dict.get(movie_id, "")
+        # metadata_all.csvから該当するdescriptionを取得します
+        option1 = item_option1_dict.get(item_id, "")
 
-        # metadata_all.csvから該当するoverviewを取得します
-        runtime = movie_runtime_dict.get(movie_id, "")
+        # metadata_all.csvから該当するdescriptionを取得します
+        option2 = item_option2_dict.get(item_id, "")
 
+        # metadata_all.csvから該当するdescriptionを取得します
+        option3 = item_option3_dict.get(item_id, "")
+        
         # 文をフォーマットします
-        formatted_sentence = sentence.format( user_id=user_id, movie_id=movie_id,profile=profile, title=title, genre=genre, overview=overview, release_date=release_date, runtime=runtime)
+        formatted_sentence = sentence.format( user_id=user_id, item_id=item_id,profile=profile, title=title, category=category, description=description, option1=option1, option2=option2, option3=option3)
 
         # リストに追加します
         sentences.append(formatted_sentence)
         labels.append(rating)
-        pairs.append(f"{user_id}_{movie_id}")
+        pairs.append(f"{user_id}_{item_id}")
 
     # 新しいDataFrameに変換します
     bert_df = pd.DataFrame({
         'sentence': sentences,
         'label': labels,
-        'userId_movieId': pairs
+        'userId_itemId': pairs
     })
 
     # 新しいCSVファイルに保存します
